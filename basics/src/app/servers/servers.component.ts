3a91a6068768086c7f4f8b0cbe9273fcdb9e45af
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-09-03 20:48:26 +0100

    Explore two-way data binding

--- basics/src/app/servers/servers.component.ts
+++ basics/src/app/servers/servers.component.ts
@@ -11,7 +11,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  serverName = '';
+  serverName = 'TestServer';
     
   constructor() {
     setTimeout(() => {
