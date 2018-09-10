5c91a5c17f05f5b6fc30cdc480e2b89aa76596d1
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-09-10 22:06:01 +0100

    Use local references in templates to pass data to controllers

--- componentsDatabinding/src/app/cockpit/cockpit.component.ts
+++ componentsDatabinding/src/app/cockpit/cockpit.component.ts
@@ -9,20 +9,26 @@ export class CockpitComponent implements OnInit {
 
     @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
     @Output() serverBlueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
-    newServerName = '';
-    newServerContent = '';
+    // newServerName = '';
+    // newServerContent = '';
 
     constructor() { }
 
     ngOnInit() {
     }
 
-    onAddServer() {
-        this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
+    onAddServer(nameElement: HTMLInputElement, contentElement: HTMLInputElement) {
+        this.serverCreated.emit({serverName: nameElement.value, serverContent: contentElement.value})
     }
+    // onAddServer() {
+    //     this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
+    // }
 
-    onAddServerBlueprint() {
-        this.serverBlueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
+    onAddServerBlueprint(nameElement: HTMLInputElement, contentElement: HTMLInputElement) {
+        this.serverBlueprintCreated.emit({serverName: nameElement.value, serverContent: contentElement.value})
     }
+    // onAddServerBlueprint() {
+    //     this.serverBlueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
+    // }
 
 }
