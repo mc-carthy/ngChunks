fd774e297eead365ecc2abeb5cf321af90fd1ad8
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-09-10 22:39:43 +0100

    Use @ViewChild to get access to dom elements

--- componentsDatabinding/src/app/cockpit/cockpit.component.ts
+++ componentsDatabinding/src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'app-cockpit',
@@ -11,22 +11,30 @@ export class CockpitComponent implements OnInit {
     @Output() serverBlueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
     // newServerName = '';
     // newServerContent = '';
+    @ViewChild('serverNameInput') serverNameInput: ElementRef;
+    @ViewChild('serverContentInput') serverContentInput: ElementRef;
 
     constructor() { }
 
     ngOnInit() {
     }
 
-    onAddServer(nameElement: HTMLInputElement, contentElement: HTMLInputElement) {
-        this.serverCreated.emit({serverName: nameElement.value, serverContent: contentElement.value})
+    onAddServer() {
+        this.serverCreated.emit({serverName: this.serverNameInput.nativeElement.value, serverContent: this.serverContentInput.nativeElement.value})
     }
+    // onAddServer(nameElement: HTMLInputElement, contentElement: HTMLInputElement) {
+    //     this.serverCreated.emit({serverName: nameElement.value, serverContent: contentElement.value})
+    // }
     // onAddServer() {
     //     this.serverCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
     // }
 
-    onAddServerBlueprint(nameElement: HTMLInputElement, contentElement: HTMLInputElement) {
-        this.serverBlueprintCreated.emit({serverName: nameElement.value, serverContent: contentElement.value})
+    onAddServerBlueprint() {
+        this.serverBlueprintCreated.emit({serverName: this.serverNameInput.nativeElement.value, serverContent: this.serverContentInput.nativeElement.value})
     }
+    // onAddServerBlueprint(nameElement: HTMLInputElement, contentElement: HTMLInputElement) {
+    //     this.serverBlueprintCreated.emit({serverName: nameElement.value, serverContent: contentElement.value})
+    // }
     // onAddServerBlueprint() {
     //     this.serverBlueprintCreated.emit({serverName: this.newServerName, serverContent: this.newServerContent})
     // }
