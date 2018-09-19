24f4d809f4146a7e0d713fabe9c8409a90d50fcd
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-09-19 21:29:04 +0100

    Set up basic app for services

--- services/src/app/app.component.ts
+++ services/src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'services';
+    accounts = [
+        {
+            name: 'Master Account',
+            status: 'Active'
+        },
+        {
+            name: 'Test Account',
+            status: 'Inactive'
+        },
+        {
+            name: 'Hidden Account',
+            status: 'Unknown'
+        },
+    ];
+
+    onAccountAdded(newAccount: { name: string, status: string }) {
+        this.accounts.push(newAccount);
+    }
+
+    onStatusChanged(updateInfo: {id: number, newStatus: string}) {
+        this.accounts[updateInfo.id].status = updateInfo.newStatus;
+    }
 }
