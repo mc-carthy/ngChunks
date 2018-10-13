20c7927dfa1fc4f68f50ae9e214abb7c5174cdc0
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-13 22:11:33 +0100

    Using canActivate route method to provide fake auth service

--- routing/src/app/app.module.ts
+++ routing/src/app/app.module.ts
@@ -11,8 +11,11 @@ import { ServersComponent } from './servers/servers.component';
 import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
-import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+import { ServersService } from './servers/servers.service';
+import { AuthGuard } from './auth-guard.service';
+import { AuthService } from './auth.service';
 
 @NgModule({
   declarations: [
@@ -31,7 +34,7 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     HttpModule,
     AppRoutingModule
   ],
-  providers: [ServersService],
+  providers: [ServersService, AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
