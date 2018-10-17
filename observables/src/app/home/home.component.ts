ca3a77c35764bbfdd0a9a3322a7d5dd28e81cd99
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-17 21:51:48 +0100

    Create custom observable

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
 import 'rxjs/Rx';
 
 @Component({
@@ -12,10 +13,37 @@ export class HomeComponent implements OnInit {
     constructor() { }
 
     ngOnInit() {
-        const myNumbers = Observable.interval(1000);
-        myNumbers.subscribe(
-            (number: number) => {
-                console.log(number);
+        // const myNumbers = Observable.interval(1000);
+        // myNumbers.subscribe(
+        //     (number: number) => {
+        //         console.log(number);
+        //     }
+        // );
+
+        const myObservable = Observable.create((observer: Observer<string>) => {
+            setTimeout(() => {
+                observer.next('First pacakge');
+            }, 2000);
+            setTimeout(() => {
+                observer.next('Second pacakge');
+            }, 4000)
+            setTimeout(() => {
+                observer.complete();
+            }, 5000)
+            // setTimeout(() => {
+            //     observer.error('Error!');
+            // }, 5000)
+        });
+
+        myObservable.subscribe(
+            (data: string) => {
+                console.log(data);
+            },
+            (error: string) => {
+                console.log(error);
+            },
+            () => {
+                console.log('Completed')
             }
         );
     }
