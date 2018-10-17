a09c5562dc3bc9dd754de45af469f42802bc3159
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-17 21:41:20 +0100

    Set up basic counting observable

--- observables/src/app/home/home.component.ts
+++ observables/src/app/home/home.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/Rx';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {
+        const myNumbers = Observable.interval(1000);
+        myNumbers.subscribe(
+            (number: number) => {
+                console.log(number);
+            }
+        );
+    }
 }
