891858b00bf3f0303724d4b9cf20010bcd2430b3
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-20 20:34:08 +0100

    Submitting and accessing form with template driven approach

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'forms';
+
+    onSubmit(form: NgForm) {
+        console.log(form);
+    }
 }
