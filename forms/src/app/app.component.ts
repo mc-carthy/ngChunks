4183488bf69c030f153e0ba15665dd0ed7328496
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-20 21:14:26 +0100

    Add two-way binding to keep updated of TD form values

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -7,7 +7,8 @@ import { NgForm } from '@angular/forms';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    defaultSecret = 'pet'
+    defaultSecret: string = 'pet';
+    answer: string = "";
 
     onSubmit(form: NgForm) {
         console.log(form);
