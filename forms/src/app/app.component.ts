4fb6ecd4ea524c0c8aceb5cccc2c29eb1e4cab9d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-20 21:46:17 +0100

    Using setValue and patchValue for TD forms

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,37 @@ import { NgForm } from '@angular/forms';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    @ViewChild('f') signupForm: NgForm;
+
     defaultSecret: string = 'pet';
     answer: string = "";
     genders = ['male', 'female']
 
     onSubmit(form: NgForm) {
         console.log(form);
+    }
+
+    onSuggestUsername() {
+        const suggestedName = "Superuser";
+
+        // The below method will set all of the form's values
+        // Patch value below, will only update the specified elements
+        // this.signupForm.setValue({
+        //     "user-data": {
+        //         username: suggestedName,
+        //         email: ''
+        //     },
+        //     "secret-data": {
+        //         secret: 'pet',
+        //         secretAnswer: ''
+        //     },
+        //     gender: 'male'
+        // });
+
+        this.signupForm.form.patchValue({
+            'user-data': {
+                username: suggestedName
+            }
+        });
     }
 }
