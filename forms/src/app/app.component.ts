2c3bcd0f79190d87748c3dc13a091460e86083ef
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-20 21:09:04 +0100

    User property binding to set default values for TD forms

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { NgForm } from '@angular/forms';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+    defaultSecret = 'pet'
 
     onSubmit(form: NgForm) {
         console.log(form);
