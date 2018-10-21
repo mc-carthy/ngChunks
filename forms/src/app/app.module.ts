b52c0cfdfa934ad120222b4c07b5d0bb20857659
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-21 09:52:31 +0100

    Set up basic form for assignment

--- forms/src/app/app.module.ts
+++ forms/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { FormAssignmentComponent } from './form-assignment/form-assignment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormAssignmentComponent
   ],
   imports: [
     BrowserModule,
