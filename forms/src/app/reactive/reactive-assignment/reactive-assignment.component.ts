3be1e205effe3e46cceed5f1e196446d0e5de543
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-21 21:30:55 +0100

    Finish reactive forms assignment

--- forms/src/app/reactive/reactive-assignment/reactive-assignment.component.ts
+++ forms/src/app/reactive/reactive-assignment/reactive-assignment.component.ts
@@ -1,4 +1,6 @@
+import { Observable } from 'rxjs';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-reactive-assignment',
@@ -7,11 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveAssignmentComponent implements OnInit {
 
-    projectStatusValues = ['Stable', 'Critical', 'Finished']
+    forbiddenProjectNames = ['Test'];
+    projectStatusValues = ['Stable', 'Critical', 'Finished'];
+    defaultProjectStatusValue = this.projectStatusValues[0];
+
+    form: FormGroup;
 
     constructor() { }
 
     ngOnInit() {
+        this.form = new FormGroup({
+            // 'projectName': new FormControl(null, this.projectNamesValidator.bind(this)),
+            'projectName': new FormControl(null, Validators.required, this.projectNamesValidatorAsync.bind(this)),
+            'email': new FormControl(null, [Validators.required, Validators.email]),
+            'projectStatus': new FormControl(this.defaultProjectStatusValue)
+        });
     }
 
+    onSubmit() {
+        console.log(this.form);
+    }
+
+    projectNamesValidator(control: FormControl) : {[s: string]: boolean} {
+        if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+            return { 'forbiddenProjectName': true };
+        }
+        return null;
+    }
+
+    projectNamesValidatorAsync(control: FormControl) : Promise<any> | Observable<any> {
+        const promise = new Promise<any>((resolve, reject) => {
+            setTimeout(() => {
+                if (this.forbiddenProjectNames.indexOf(control.value) !== -1) {
+                    resolve({ 'forbiddenProjectName': true });
+                } else {
+                    resolve(null);
+                }
+            }, 1500);
+        });
+        return promise;
+    }
 }
