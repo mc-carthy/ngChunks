0350f9b4501fa156ee8dbdd462ec02568922dccc
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-10-24 23:58:18 +0100

    Use the async pipe for promises

--- pipes/src/app/pipe-example/pipe-example.component.ts
+++ pipes/src/app/pipe-example/pipe-example.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PipeExampleComponent implements OnInit {
 
+    appStatus = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('stable');
+        }, 2000);
+    });
+
     filteredStatus = '';
 
     servers = [
